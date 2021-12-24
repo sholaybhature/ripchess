015996ecc6b722d03c65c08dff5c5827dcf83de1
Saurabh <b.saur@pm.me>
2021-12-24 19:49:05 +0530

    tracking of other pieces and pawn promotion and castling

--- src/App.js
+++ src/App.js
@@ -1,59 +1,114 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Chess from "./chess2";
+import boardNotation from "./board";
+import {game1, game1Captures} from './testLogic'
 // import * as Chess from "chess"
+// todo: wirte a test, reformat code, remove additions from chess.js
+// todo: handle for pawn promotions and castling
 let chess = new Chess();
-// let chessGame = "d4 d5 e4 dxe4 f3 exf3 gxf3 e5 dxe5 f6 exf6 gxf6 f4 f5"
-let chessGame = "d4 d5 e4 dxe4 f3 exf3 gxf3 e5 dxe5 f6 exf6 gxf6 f4 f5"
-let chessMoves = chessGame.split(" ")
-let chessPieces = {}
-let capturingPawns = {}
-let finalCapturedPawns = {}
-let capturedPiece
-for (let i =0; i < chessMoves.length; i++) {
-  let currMove = chess.move(chessMoves[i])
-  let piece = currMove.color + currMove.file
-  Object.entries(chessPieces).forEach(([k, val])=>{
-    for (let j =0; j<val.length;j++){
-      if (val[j]==currMove.from){
-        piece = k
-      }
+// let chessGame = "d4 d5 e4 e5 Qg4 f5 Bf4 c5 Nc3 b5 O-O-O exd4 Rxd4";
+let chessGame = "e4 e5 Nf3 Nc6 a3 Bc5 g3 Nf6 Nc3 O-O Bg2 d6 O-O Bg4 d3 a5 Nd5 Nxd5 exd5 Nd4 Be3 a4 c3 Nb3 Rb1 c6 d4 Bb6 dxe5 Bxe3 fxe3 dxe5 Qd3 Qd6 Nd2 Bf5 Rxf5 g6 Nc4 Qc5 Rxe5 Rad8 d6 b5 Rxc5 Nxc5 Qd4 Ne6 Qa7 bxc4 Rd1 f5 Bxc6 g5 d7 f4 exf4 gxf4 Bd5 Rfe8 dxe8=Q+ Rxe8 Bxc4 f3 Rf1 Rf8 Qxa4 Kf7 Rxf3+ Ke7 Rxf8 Kxf8 Bxe6 Ke7 Qc6 Kf6 a4 h5 a5 h4 a6 h3 a7 Kg5 a8=Q Kf6 Qf8+ Kg5 Qcf3 Kg6 Qg8+ Kh6 Qf6+ Kh5 Qgg5#"
+// let chessGame = "e4 e5 Nf3 Nc6 a3 Bc5 g3 Nf6 Nc3 O-O Bg2 d6 O-O Bg4 d3 a5 Nd5 Nxd5 exd5 Nd4 Be3 a4 c3 Nb3 Rb1 c6 d4 Bb6 dxe5 Bxe3 fxe3 dxe5 Qd3 Qd6 Nd2 Bf5 Rxf5 g6 Nc4 Qc5 Rxe5 Rad8 d6 b5 Rxc5 Nxc5 Qd4 Ne6 Qa7 bxc4 Rd1 f5 Bxc6 g5 d7 f4 exf4 gxf4 Bd5 Rfe8 dxe8=Q+"
+let chessMoves = chessGame.split(" ");
+let chessPieces = {};
+let finalCapturedPawns = {};
+let capturedPiece;
+for (let i = 0; i < chessMoves.length; i++) {
+    let currMove = chess.move(chessMoves[i]);
+    let piece = currMove.color + currMove.piece + currMove.file;
+    // console.log(piece)
+    // castling
+    if (currMove.san == "O-O") {
+      let rookPiece = currMove.color + "rh"
+      chessPieces[rookPiece] = currMove.color == "w" ? ["f1"] : ["f8"]
     }
-  })
-  if (currMove.flags == "c") {
-    let capturedColor = currMove.color == "w" ? "b" : "w"
-    let capturedFile = currMove.tofile
-    capturedPiece = capturedColor + capturedFile
-
-    try {
-      finalCapturedPawns[capturedPiece] = chessPieces[capturedPiece].at(-1)
-      delete chessPieces[capturedPiece]
+    if (currMove.san == "O-O-O") {
+      let rookPiece = currMove.color + "ra"
+      chessPieces[rookPiece] = currMove.color == "w" ? ["d1"] : ["d8"]
     }
-    catch (err) {
-      Object.entries(chessPieces).forEach(([k, val])=>{
-        for (let j =0; j<val.length;j++){
-          if (val[j]==currMove.to){
-            capturedPiece = k
-          }
+    // if the moved file is different, suppose Re4 -> Rf4. 
+    Object.entries(chessPieces).forEach(([k, val]) => {
+        let lastPiece = k.charAt(1);
+        for (let j = 0; j < val.length; j++) {
+            if (val[j] == currMove.from && lastPiece == currMove.piece) {
+                piece = k;
+            }
         }
-      })
-      finalCapturedPawns[capturedPiece] = chessPieces[capturedPiece].at(-1)
-      delete chessPieces[capturedPiece]
+    });
+    // console.log(piece)
+    if (
+        currMove.flags == "c" ||
+        currMove.flags == "e" ||
+        currMove.flags == "p" ||
+        currMove.flags == "cp"
+    ) {
+        let capturedColor = currMove.color == "w" ? "b" : "w";
+        let capturedFile = currMove.tofile;
+        let capturedPieceCode = currMove.captured;
+        capturedPiece = capturedColor + capturedPieceCode + capturedFile;
+        // if captured piece file is different
+        Object.entries(chessPieces).forEach(([k, val]) => {
+            // since en passant jumps twice, there might be a piece prior where the
+            // capturing pawn will go.
+            if (
+                currMove.flags == "c" ||
+                currMove.flags == "p" ||
+                currMove.flags == "cp"
+            ) {
+                for (let j = 0; j < val.length; j++) {
+                    if (val[j] == currMove.to) {
+                        capturedPiece = k;
+                    }
+                }
+            }
+        });
+
+        // if piece is captured at original location
+        if (!chessPieces[capturedPiece]) {
+            console.log(1);
+            console.log(piece, capturedPiece)
+            finalCapturedPawns[boardNotation[capturedPiece]] = currMove.to;
+        } else {
+            finalCapturedPawns[boardNotation[capturedPiece]] =
+                chessPieces[capturedPiece].at(-1);
+            delete chessPieces[capturedPiece];
+        }
     }
-  }
-  console.log(piece,capturedPiece)
-  if (chessPieces[piece]) {
-    chessPieces[piece].push(currMove.to)
-  } else {
-    chessPieces[piece] = [currMove.to]
+    if (chessPieces[piece]) {
+        chessPieces[piece].push(currMove.to);
+    } else {
+        chessPieces[piece] = [currMove.to];
+    }
+    // if a pawn is promoted, it's over for that pawn, no need to track the promoted piece
+    if (currMove.flags == "cp" || currMove.flags == "p") {
+      if (!finalCapturedPawns[boardNotation[piece]]) {
+        finalCapturedPawns[boardNotation[piece]] = chessPieces[piece].at(-1);
+        delete chessPieces[piece];
+      }
   }
 }
+// if promoted piece gets captured later, it gives undefined as boardnotation doesn't have notation for it, so remove that undefined
+Object.keys(finalCapturedPawns).forEach((key) => {
+  if (key=="undefined"){
+    delete finalCapturedPawns[key]
+  }
+})
 
-let chessHistory = chess.history({verbose:true})
-console.log(chessHistory)
+if (chess.in_checkmate()) {
+    let king = chess.turn() + "ke";
+    if (chessPieces[king]) {
+        finalCapturedPawns[boardNotation[king]] = chessPieces[king].at(-1);
+    } else {
+        finalCapturedPawns[boardNotation[king]] =
+            chess.turn() == "b" ? "e1" : "e8";
+    }
+}
+let chessHistory = chess.history({ verbose: true });
+console.log(chessHistory);
 // console.log(chessCaptures)
-console.log(chessPieces)
-console.log(finalCapturedPawns)
+console.log(chessPieces);
+console.log(finalCapturedPawns);
 function App() {
     return <div className="App"></div>;
 }
---
+++ src/testLogic.js
@@ -0,0 +1,26 @@
+// https://lichess.org/752ShqAw#17
+const game1 = "e4 e5 Nf3 Nc6 a3 Bc5 g3 Nf6 Nc3 O-O Bg2 d6 O-O Bg4 d3 a5 Nd5 Nxd5 exd5 Nd4 Be3 a4 c3 Nb3 Rb1 c6 d4 Bb6 dxe5 Bxe3 fxe3 dxe5 Qd3 Qd6 Nd2 Bf5 Rxf5 g6 Nc4 Qc5 Rxe5 Rad8 d6 b5 Rxc5 Nxc5 Qd4 Ne6 Qa7 bxc4 Rd1 f5 Bxc6 g5 d7 f4 exf4 gxf4 Bd5 Rfe8 dxe8=Q+ Rxe8 Bxc4 f3 Rf1 Rf8 Qxa4 Kf7 Rxf3+ Ke7 Rxf8 Kxf8 Bxe6 Ke7 Qc6 Kf6 a4 h5 a5 h4 a6 h3 a7 Kg5 a8=Q Kf6 Qf8+ Kg5 Qcf3 Kg6 Qg8+ Kh6 Qf6+ Kh5 Qgg5#"
+const game1Captures = {
+    'b1': 'd5',
+    'g8': 'd5',
+    'e7': 'e5',
+    'c1': 'e3',
+    'f8': 'e3',
+    'd2': 'e5',
+    'c8': 'f5',
+    'd7': 'e5',
+    'd8': 'c5',
+    'h1': 'c5',
+    'g1': 'c4',
+    'c7': 'c6',
+    'f7': 'f4',
+    'h8': 'e8',
+    'b7': 'c4',
+    'a7': 'a4',
+    'g7': 'f3',
+    'a8': 'f8',
+    'a1': 'f8',
+    'b8': 'e6',
+}
+
+export {game1, game1Captures}
