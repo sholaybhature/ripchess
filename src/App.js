bd205242ce1b96f88b8a2f0fcf45aea26a34cfb3
Divyam Hasija <divyamhasija113@gmail.com>
2021-12-29 00:43:50 +0530

    code reformated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Lichess from "./lichess";
+import { getGames } from "./lichess";
 
 import * as Chess from "chess.js"
 import {processGame, checkCode} from './ripchess'
 // processGame("e3 e5 Nf3 Nc6 Bc4 d5 Bb3 Be6 c4 Nf6 cxd5 Bxd5 Bxd5 Nxd5 d4 e4 Ne5 Nxe5 dxe5 Qg5 Qa4+ c6 b4 Nxb4 Qxb4 Bxb4+")
-checkCode()
+//checkCode()
+getGames();
 function App() {
     return <div className="App">
         <h1>ripchess</h1>
-        <Lichess />
     </div>;
 }
 
