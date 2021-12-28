ad8723247c885953318ba5f6efbf9fec1ac45430
Divyam Hasija <divyamhasija113@gmail.com>
2021-12-29 00:45:44 +0530

    api added

--- src/lichess.js
+++ src/lichess.js
@@ -1,24 +1,17 @@
-import React from 'react'
-import * as Chess from 'chess.js'
-
+import { passMoves } from './ripchess'
 
 const options = {
     method : 'GET',
     headers: new Headers({'accept': 'application/x-ndjson'}),
 }
 
-async function getGames() {
-    let response = await fetch(`https://lichess.org/api/games/user/sp1nalcord?max=2`, options);
+export const getGames = async () => {
+    let response = await fetch(`https://lichess.org/api/games/user/sp1nalcord?max=7`, options);
     let data = (await response.text()).match(/.+/g).map(JSON.parse);
-    console.log(data);
+    var movesArray = data.map(obj => obj.moves);
+    // for (let i=0; i<movesArray.length; i++) {
+    //     movesArray[i] = [movesArray[i]];
+    // }
+    console.log(movesArray);
+    passMoves(movesArray);
 }
-getGames();
-function Lichess() {
-    return (
-        <div>
-            <h2>HELLO</h2>
-        </div>
-    )
-}
-
-export default Lichess;
