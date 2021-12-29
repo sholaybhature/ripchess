2cb49e96f481a4797079faa6bf4d76a37eeeb954
Monark Jain <jainmonark145@gmail.com>
2021-12-30 00:10:23 +0530

    initiated chesscom

---
+++ src/chesscom.js
@@ -0,0 +1,53 @@
+ import { passMoves } from './ripchess'
+
+    // const options = {
+    //     method : 'GET',
+    //     headers: new Headers({'accept': 'application/json'}),
+    //     mode: 'no-cors'
+    // }
+
+    const options= {
+        method:'GET',
+        mode: 'cors',
+        headers:{
+        'Access-Control-Allow-Origin':'*'
+    },
+}
+const axios = require('axios');
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios({
+    method: 'get',
+    url: 'https://www.chess.com/callback/live/game/28911003455',
+  })
+    .then(function (response) {
+      console.log(response);
+    });
+    
+    export const getGames = async () => {
+        let response = await fetch(`https://www.chess.com/callback/live/game/28911003455`, options);
+        let data = await response.json();
+        // var movesArray = data.map(obj => obj.moves);
+        // for (let i=0; i<movesArray.length; i++) {
+        //     movesArray[i] = [movesArray[i]];
+        // }
+        console.log(data);
+        // passMoves(movesArray);
+    }
+var ChessWebAPI = require('chess-web-api');
+
+var chessAPI = new ChessWebAPI();
+
+chessAPI.getPlayerMonthlyArchives('monarkjain')
+    .then(function(response) {
+        console.log('Player Profile', response.body);
+    }, function(err) {
+        console.error(err);
+    });
+chessAPI.getPlayerCompleteMonthlyArchives('monarkjain',2021,10)
+    .then(function(response) {
+        console.log('Player Profile', response.body);
+    }, function(err) {
+        console.error(err);
+    });
+
+   
