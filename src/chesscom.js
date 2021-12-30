ff33823c38ec2a5ef3eb98e473a91be349452878
saurabh <b.saur@pm.me>
2021-12-31 00:04:16 +0530

    reformatted code

--- src/chesscom.js
+++ src/chesscom.js
@@ -1,37 +1,10 @@
 import { useState, useEffect } from "react";
 export function ChessComAPI(props) {
-  const [isLoaded, setIsLoaded] = useState(false);
   const [monthlyArchives, setMonthlyArchives] = useState([]);
-  const [gameArchives, setGameArchives] = useState([]);
   // const BASEAPI = "https://api.chess.com/pub/player/monarkjain/games/archive"
   const BASEAPI = `https://api.chess.com/pub/player/${props.username}`;
   const monthlyArchiveURL = BASEAPI + "/games/archives";
-  const baseGamesArchiveURL = BASEAPI + "/games/";
-  let promisesArr = [];
-  const fetchMonthlyGames = async (arr) => {
-    let promisesArrr = arr.map((item) => {
-      return fetch(item)
-        .then((res) => res.json())
-        .then((res) => res.games);
-    });
-    console.log(promisesArrr);
-    return await Promise.all(promisesArrr);
-    // const r = fetch(baseGamesArchiveURL + "2020/07");
-    // return await r.then((res) => res.json());
-  };
 
-  useEffect(() => {
-    fetch(monthlyArchiveURL)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setMonthlyArchives(result.archives);
-        },
-        (error) => {
-          console.log("error in fetching monthlyArchives: ", error);
-        }
-      );
-  }, []);
   const fetchAllGames = async (url) => {
     const res = await fetch(url);
     const json = await res.json();
@@ -49,22 +22,6 @@ export function ChessComAPI(props) {
   useEffect(() => {
     const result = fetchAllGames(monthlyArchiveURL);
     console.log(result);
-    // let games = fetchMonthlyGames(monthlyArchives);
-    // console.log(games);
-    // console.log(r.then((r) => console.log(r.games)));
-    // fetch(baseGamesArchiveURL + "2020/07")
-    // .then((res) => res.json())
-    // .then(
-    // (result) => {
-    // setGameArchives(result);
-    // },
-    // (error) => {
-    // console.log("error in fetching gameArchives: ", error);
-    // }
-    // );
   }, [monthlyArchives]);
-  // console.log(monthlyArchives);
-  // console.log(gameArchives);
-  // fetchAllGames(monthlyArchiveURL);
   return <></>;
 }
