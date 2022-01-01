972d0bd481b7753a239ea678e654d5bac1375a15
saurabh <b.saur@pm.me>
2022-01-02 01:56:34 +0530

    reformatted a bit

--- src/lichess.js
+++ src/lichess.js
@@ -1,38 +1,24 @@
 import { processGame } from "./ripchess";
 import ndjsonStream from "can-ndjson-stream";
-import { chessPieces } from "./board";
+import { finalPieces } from "./utility";
 const options = {
   method: "GET",
   headers: new Headers({ accept: "application/x-ndjson" }),
 };
-// TODO: Add a util function for chessPieces.
-export const fetchLiChessCom = async () => {
-  var count = 0;
-  const t0 = performance.now();
-  let response = await fetch(
-    // `https://lichess.org/api/games/user/sp1nalcord?pgnInJson=true`,
-    `https://lichess.org/api/games/user/sp1nalcord?max=7`,
-    options
-  );
+// Fetch all games from Lichess
+export const fetchLichessCom = async (username) => {
+  let fetchURL = `https://lichess.org/api/games/user/${username}?pgnInJson=true`;
+  let res;
+  let d = {};
+  // let fetchURL = `https://lichess.org/api/games/user/${username}?max=7`;
+  let response = await fetch(fetchURL, options);
   // Streaming the ndjson response
   const reader = ndjsonStream(response.body).getReader();
   while (true) {
     const { value, done } = await reader.read();
-    console.log(value);
     if (done) break;
-    let game = value.moves;
-    let resGame = processGame(game);
-    count = count + 1;
-    // console.log(resGame);
-    Object.entries(resGame).forEach(([key, value]) => {
-      if (chessPieces[key][value]) {
-        chessPieces[key][value] += 1;
-      } else {
-        chessPieces[key][value] = 1;
-      }
-    });
+    res = processGame(value.moves);
+    finalPieces(res, d);
   }
-  const t1 = performance.now();
-  console.log(`It took ${t1 - t0} milliseconds`);
-  console.log(chessPieces, count);
+  return d;
 };
