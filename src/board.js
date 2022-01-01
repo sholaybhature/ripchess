f7d03bb97c97b38b1a6300c15015993f382b0f5b
saurabh <b.saur@pm.me>
2022-01-02 01:55:19 +0530

    removed the chess pieces dict

--- src/board.js
+++ src/board.js
@@ -33,40 +33,4 @@ let boardNotation = {
   bph: "h7",
 };
 
-let chessPieces = {
-  a1: {},
-  b1: {},
-  c1: {},
-  d1: {},
-  e1: {},
-  f1: {},
-  g1: {},
-  h1: {},
-  a2: {},
-  b2: {},
-  c2: {},
-  d2: {},
-  e2: {},
-  f2: {},
-  g2: {},
-  h2: {},
-  a8: {},
-  b8: {},
-  c8: {},
-  d8: {},
-  e8: {},
-  f8: {},
-  g8: {},
-  h8: {},
-  a7: {},
-  b7: {},
-  c7: {},
-  d7: {},
-  e7: {},
-  f7: {},
-  g7: {},
-  h7: {},
-};
-
-export { boardNotation, chessPieces };
-
+export { boardNotation };
