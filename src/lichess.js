4a7ae5e55b1488b718de89c17a9b322113079b6c
saurabh <b.saur@pm.me>
2022-01-01 15:40:28 +0530

    reformatted api streaming

--- src/lichess.js
+++ src/lichess.js
@@ -5,25 +5,12 @@ const options = {
   method: "GET",
   headers: new Headers({ accept: "application/x-ndjson" }),
 };
-
+// TODO: Fetch all games, instead of just 30.
 export const fetchLiChessCom = async () => {
-  const t0 = performance.now();
   let response = await fetch(
     `https://lichess.org/api/games/user/sp1nalcord?max=7`,
     options
   );
-  // let data = (await response.text()).match(/.+/g).map(JSON.parse);
-  // for (let i = 0; i < data.length; i++) {
-  // let game = data[i].moves;
-  // let resGame = processGame(game);
-  // Object.entries(resGame).forEach(([key, value]) => {
-  // if (chessPieces[key][value]) {
-  // chessPieces[key][value] += 1;
-  // } else {
-  // chessPieces[key][value] = 1;
-  // }
-  // });
-  // }
   // Streaming the ndjson response
   const reader = ndjsonStream(response.body).getReader();
   while (true) {
@@ -41,14 +28,4 @@ export const fetchLiChessCom = async () => {
     });
   }
   console.log(chessPieces);
-  const t1 = performance.now();
-  console.log(`It took ${t1 - t0} milliseconds`);
-  // let data = (await response.text()).match(/.+/g).map(JSON.parse);
-  // console.log(data);
-  // var movesArray = data.map(obj => obj.moves);
-  // for (let i=0; i<movesArray.length; i++) {
-  //     movesArray[i] = [movesArray[i]];
-  // }
-  // console.log(movesArray);
-  // passMoves(movesArray);
 };
