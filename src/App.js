0aebf7237a0314582a8ca766e101ed1ecd430325
Divyam Hasija <divyamhasija113@gmail.com>
2022-01-06 01:08:57 +0530

    heatmap component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import { fetchChessCom } from "./chesscom";
 import { fetchLichessCom } from "./lichess";
+import Heatmap from './heatmap';
 // import { fetchChessCom } from "./chesscom2";
 // let username = "cjasnbckjanck";
 let username = "monarkjain";
 let username2 = "sp1nalcord";
 // let res = fetchChessCom(username);
-let res = fetchLichessCom(username2);
-res.then((i) => console.log(i));
+// let res = fetchLichessCom(username2);
+// console.log(res);
+// res.then((i) => console.log(i));
 function App() {
   return (
     <div className="App">
       <h1>ripchess</h1>
+      <Heatmap />
     </div>
   );
 }
