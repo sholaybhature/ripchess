cf275be7416039b8a68974611a130ff9bb4005d3
Saurabh <b.saur@pm.me>
2022-01-13 21:10:02 +0530

    added search input box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Heatmap } from "./heatmap";
 import { ParentSize } from "@visx/responsive";
+import Search from "./components/search";
 function App() {
   return (
-    <div className="App" style={{ height: "30vh", width: "15vw" }}>
-      <ParentSize>
-        {({ width, height }) => <Heatmap width={width} height={height} />}
-      </ParentSize>
-      <h1>ripchess</h1>
+    <div className="parent-container">
+      <Search></Search>
     </div>
+    // <ParentSize>
+    // {({ width, height }) => <Heatmap width={width} height={height} />}
+    // </ParentSize>
   );
 }
 
